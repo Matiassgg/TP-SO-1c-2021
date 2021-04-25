b4cd4ef7b0be53ee0545e1224d801b600262300d
tomasberrojalviz <cai.berrojalviz.tomas@gmail.com>
2021-04-25 18:11:53 -0300

    Se agrega utils para ram

--- Mi-RAM-HQ/src/Mi-RAM-HQ.h
+++ Mi-RAM-HQ/src/Mi-RAM-HQ.h
@@ -1,28 +1,10 @@
-/*
- * Mi-RAM-HQ.h
- *
- *  Created on: 25 abr. 2021
- *      Author: utnso
- */
-
 #ifndef MI_RAM_HQ_SRC_MI_RAM_HQ_H_
 #define MI_RAM_HQ_SRC_MI_RAM_HQ_H_
 
 #include <utils/hello.h>	// Incluyo la shared
 
-char* ip_Mi_RAM_HQ;
-char* puerto_escucha;
-char* punto_montaje;
-char* archivo_log;
-
-t_config* config;
-t_log* logger;
-pthread_t hiloReceive;
 
 void iniciar_Mi_RAM_HQ(void);
 void leer_config(void);
-void arrancar_servidor();
-void serve_client(int* socketQuenosPAsan);
-void procesar_mensaje_recibido(int cod_op, int cliente_fd);
 
 #endif /* MI_RAM_HQ_SRC_MI_RAM_HQ_H_ */
---
+++ Mi-RAM-HQ/src/utils_ram.h
@@ -0,0 +1,19 @@
+#ifndef MI_RAM_HQ_SRC_UTILS_RAM_H_
+#define MI_RAM_HQ_SRC_UTILS_RAM_H_
+
+#include <utils/hello.h>	// Incluyo la shared
+
+char* ip_Mi_RAM_HQ;
+char* puerto_escucha;
+char* punto_montaje;
+char* archivo_log;
+
+t_config* config;
+t_log* logger;
+pthread_t hiloReceive;
+
+void arrancar_servidor();
+void serve_client(int* socketQuenosPAsan);
+void procesar_mensaje_recibido(int cod_op, int cliente_fd);
+
+#endif /* MI_RAM_HQ_SRC_UTILS_RAM_H_ */
