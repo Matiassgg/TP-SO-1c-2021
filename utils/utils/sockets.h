36585750f666e1c7426c7d056a4d01ac76266437
Matiassgg <matugrillo@hotmail.com>
2021-04-27 09:54:44 -0300

    Cambio la firma de liberar_conexion(...)

--- utils/utils/sockets.h
+++ utils/utils/sockets.h
@@ -1,16 +1,18 @@
 #ifndef SOCKETS_H_
 #define SOCKETS_H_
 
-#include<sys/socket.h>
-#include<unistd.h>
-#include<netdb.h>
-#include<commons/log.h>
-#include<string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <commons/log.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int iniciar_servidor(t_log* logger, char* ip, char* puerto);
 int esperar_cliente(t_log* logger, int socket_servidor);
 uint32_t conectar(t_log* logger, char* ip, char* puerto);
 int crear_conexion(t_log* logger, char *ip, char* puerto);
-void liberar_conexion(int* socket_cliente);
+void liberar_conexion(uint32_t* socket_cliente);
 
 #endif
