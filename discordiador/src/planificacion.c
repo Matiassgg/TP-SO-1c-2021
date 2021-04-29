67fbdde82ff730c355dc25a90a691ed920bc8b97
Matiassgg <matugrillo@hotmail.com>
2021-04-29 00:10:23 -0300

    validacion de algoritmo y colas e/s y emergencia

--- discordiador/src/planificacion.c
+++ discordiador/src/planificacion.c
@@ -1,9 +1,17 @@
 #include "planificacion.h"
 
-void iniciar_planificacion(void){
+void iniciar_planificacion(){
 	cola_new = queue_create();
 	cola_ready = queue_create();
 	cola_exec = queue_create();
 	cola_exit = queue_create();
-	cola_bloq = queue_create();
+	cola_bloq_E_S = queue_create();
+	cola_bloq_Emergencia = queue_create();
+
+	if(algoritmo == NULL)
+		log_info(logger, "Error al leer el algoritmo de planificacion");
+	else if(sonIguales(algoritmo, "RR"))
+		log_info(logger, "El algoritmo seleccionado es: %s con quantum de %d", algoritmo, quantum);
+	else
+		log_info(logger, "El algoritmo seleccionado es: %s", algoritmo);
 }
--- discordiador/src/utils_discordiador.h
+++ discordiador/src/utils_discordiador.h
@@ -1,7 +1,7 @@
 #ifndef DISCORDIADOR_SRC_UTILS_DISCORDIADOR_H_
 #define DISCORDIADOR_SRC_UTILS_DISCORDIADOR_H_
 
-#include <utils/hello.h>	// Incluyo la shared
+#include <utils/hello.h>
 
 char* ip_discordiador;
 char* puerto_escucha;
@@ -17,6 +17,9 @@ int socket_Mongo_Store;
 char* punto_montaje;
 char* archivo_log;
 
+char* algoritmo;
+uint32_t quantum;
+
 t_config* config;
 t_log* logger;
 pthread_t hiloReceive;
@@ -25,7 +28,9 @@ t_queue* cola_new;
 t_queue* cola_ready;
 t_queue* cola_exec;
 t_queue* cola_exit;
-t_queue* cola_bloq;
+// Hay 2 tipos de colas de bloqueados: E/S y Sabotaje o Emergencia
+t_queue* cola_bloq_E_S;
+t_queue* cola_bloq_Emergencia;
 
 void arrancar_servidor();
 void serve_client(int* socketQuenosPAsan);
