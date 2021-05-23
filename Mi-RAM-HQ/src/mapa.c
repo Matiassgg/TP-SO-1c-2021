4a38517bf186ddbbfda4421f2d44fa7373675f0a
Matias Grillo <matugrillo@hotmail.com>
2021-05-23 03:47:16 -0300

    Me falto el caracter (?

--- Mi-RAM-HQ/src/mapa.c
+++ Mi-RAM-HQ/src/mapa.c
@@ -33,6 +33,6 @@ void iniciar_mapa_vacio() {
 }
 
 int crear_tripulante(NIVEL* nivel, t_tripulante* tripulante){
-	return personaje_crear(nivel, tripulante->id, tripulante->posicion.pos_x, tripulante->posicion.pos_y);
+	return personaje_crear(nivel, tripulante->id + '0', tripulante->posicion.pos_x, tripulante->posicion.pos_y);
 }
 
