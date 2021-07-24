97e43785a858213d0fd955d17f19f9b7b05f9166
Marcio Garozzo <mgarozzo@est.frba.utn.edu.ar>
2021-07-23 21:58:18 -0300

    Agrego algoritmos para resolver sabotajes (algunos)

--- i-Mongo-Store/src/sabotaje.h
+++ i-Mongo-Store/src/sabotaje.h
@@ -2,10 +2,10 @@
 #define I_MONGO_STORE_SRC_SABOTAJE_H_
 
 #include "utils_mongo.h"
-
+#include "dirent.h"
 
 void resolver_sabotaje_files_blocks(char*);
 void resolver_sabotaje_files_blockcount(char*);
-
+char* obtener_path_bitacora(char* nombreBitacora);
 
 #endif /* I_MONGO_STORE_SRC_SABOTAJE_H_ */
